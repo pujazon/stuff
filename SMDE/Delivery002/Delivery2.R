abfe4de102b5e024bf638b7832d4dfc291f91f85
Daniel Pujazon Bonet <danielpujazonbonet@gmail.com>
2021-04-20 21:29:10 +0100

    Delivery 002 eercice 1 and 2

--- SMDE/Delivery002/Delivery2.R
+++ SMDE/Delivery002/Delivery2.R
@@ -47,8 +47,6 @@ if (Ftest > critcalV){
   cat("Null hypothesis true\n")
 }
 
-#Donde with same mean says that's true. 
-
 #Also check it through AnovaModel
 df1=data.frame(x1=d1, x2="d1")
 df2=data.frame(x1=d2, x2="d2")
@@ -56,6 +54,7 @@ df3=data.frame(x1=d3, x2="d3")
 
 data=mergeRows(df1, df2, common.only=FALSE)
 data=mergeRows(as.data.frame(data), df3, common.only=FALSE)
+
 ### ANOVA on simulated data ####
 AnovaModel.1 <- aov(x1 ~ x2, data=data)
 summary(AnovaModel.1)
@@ -68,7 +67,14 @@ shapiro.test(residuals(AnovaModel.1))
 #The populations from which the samples are selected must have equal variances (homogeneity of variance)
 bptest(AnovaModel.1)
 
+#########################################################
 #Red and White Wine Quality
+#########################################################
+#Libaries
+library(RcmdrMisc)
+library(lmtest)
+
+#Read DataSet
 options(digits=2)
 redDS<-read.csv2("winequality-red.csv", sep=",", dec=".")
 rRows<-nrow(redDS)
@@ -86,4 +92,102 @@ RW <- rbind(redDS,whiteDS)
 RW$WineQuality<- cut(RW$quality,c(1,5,6,10))
 levels(RW$WineQuality)<-c("Low","Medium","High")
 
+#Variables and constants
+qualityGroups=3
+iLowData=1
+iMedData=2
+iHighData=3
+iRedData=1
+iWhiteData=2
+jFirstContVar=1
+jLastContVar=13
+jType=14
+jQuality=15
+jlast=15
+DataArray<-matrix(0,qualityGroups,wIdEnd)
 
+print("************************")
+print("Ex 1) Quality")
+print("************************")
+for (chemic in 1:1) {
+  print("************************")
+  print(paste0("See if it's influenced by: ", chemic))
+  
+  #Clean idx var
+  DataArray<-matrix(0,qualityGroups,wIdEnd)
+  lastLowData=1
+  lastMedData=1
+  lastHighData=1
+  
+  for (id in 1:wIdEnd) {
+    iQuality<-RW[id,jQuality]
+    if(iQuality == "Low") {
+      DataArray[iLowData,lastLowData]<-RW[id,chemic]
+      lastLowData<-(lastLowData+1)
+    } else if (iQuality == "Medium") {
+      DataArray[iMedData,lastMedData]<-RW[id,chemic]
+      lastMedData<-(lastMedData+1)
+    } else if (iQuality == "High") {
+      DataArray[iHighData,lastHighData]<-RW[id,chemic]
+      lastHighData<-(lastHighData+1)
+    } else {
+      print("Error")
+      exit()
+    }
+  }
+
+  dfL=data.frame(x1=DataArray[iLowData,], x2="Low")
+  dfM=data.frame(x1=DataArray[iMedData,], x2="Medium")
+  dfH=data.frame(x1=DataArray[iHighData,], x2="High")
+  data=mergeRows(dfL, dfM, common.only=FALSE)
+  data=mergeRows(as.data.frame(data), dfH, common.only=FALSE)
+   
+  print(paste0("Anova result: ", chemic))
+  AnovaModel.1 <- aov(x1 ~ x2, data=data)
+  print(summary(AnovaModel.1))
+    
+  print(paste0("Check Anova assumptions: ", chemic))
+  print(dwtest(AnovaModel.1, alternative ="two.sided"))
+  #shapiro.test(residuals(AnovaModel.1))
+  print(bptest(AnovaModel.1))
+}
+
+print("************************")
+print("Ex 2) Type")
+print("************************")
+for (chemic in 1:jLastContVar) {
+  print("************************")
+  print(paste0("See if it's influenced by: ", chemic))
+  
+  #Clean idx var
+  DataArray<-matrix(0,qualityGroups,wIdEnd)
+  lastRedData=1
+  lastWhiteData=1
+  
+  for (id in 1:wIdEnd) {
+    iType<-RW[id,jType]
+    if(iType == "Red") {
+      DataArray[iRedData,lastRedData]<-RW[id,chemic]
+      lastRedData<-(lastRedData+1)
+    } else if (iType == "White") {
+      DataArray[iWhiteData,lastWhiteData]<-RW[id,chemic]
+      lastWhiteData<-(lastWhiteData+1)
+    } else {
+      print("Error")
+      exit()
+    }
+  }
+  
+  dfR=data.frame(x1=DataArray[iRedData,], x2="Red")
+  dfW=data.frame(x1=DataArray[iWhiteData,], x2="White")
+  data=mergeRows(dfR, dfW, common.only=FALSE)
+  
+  print(paste0("Anova result: ", chemic))
+  AnovaModel.1 <- aov(x1 ~ x2, data=data)
+  print(summary(AnovaModel.1))
+  
+  print(paste0("Check Anova assumptions: ", chemic))
+  print(dwtest(AnovaModel.1, alternative ="two.sided"))
+  ks.test(x=residuals(AnovaModel.1),y='pnorm',alternative='two.sided')
+  print(bptest(AnovaModel.1))
+}
